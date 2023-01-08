75bd35457f4aeb5a2020fcf8fc840014c68f205f
Verrgod <s10206142@connect.np.edu.sg>
2023-01-09 01:11:24 +0800

    Queries for /companies

--- backend/src/routes/companies.js
+++ backend/src/routes/companies.js
@@ -1,5 +1,15 @@
-const express = require("express")
+const express = require('express')
 const router = express.Router()
+
+const XLSX = require('xlsx')
+
+const mysql = require('mysql')
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'user',
+    password: 'password',
+    database: 'db'
+})
 
 // middleware
 router.use((req, res, next) => {
@@ -8,11 +18,37 @@ router.use((req, res, next) => {
 })
 
 router.get('/', (req, res) => {
-    res.send('Company List')
+    con.connect(error => {
+        if (error) throw error
+        console.log('Connected!')
+
+        con.query('SELECT * FROM company', (error, results) => {
+            if (error) throw error
+            res.send(results)
+        })
+    })
+    con.end()
 })
 
 router.post('/upload', (req, res) => {
-    res.send('Upload company data')
+    con.connect(error => {
+        if (error) throw error
+        console.log('Connected!')
+
+        const companyDataExcel = XLSX.readFile('./././database/internship data/<int period>/student<datetime>.xlsx')
+        const companyData = companyDataExcel.companyDataExcel[companyData.SheetNames[0]]
+
+        // loop through rows
+        const rows = XLSX.utils.sheet_to_json(companyData, { header: 1 })
+        rows.array.forEach(row => {
+            // insert row into sql
+            const query = "INSERT INTO companyData (column1, column2, ...) VALUES (?, ?, ...) ON DUPLICATE KEY UPDATE column1 = ?, column2 = ?, ..."
+            con.query(query, [...row, ...row], (error, result) => {
+                if (error) throw error
+            })
+        });
+        con.end()
+    })
 })
 
 module.exports = router
