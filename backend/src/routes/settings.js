3c9b91efd04f967dfbf7fac2df8d2da061275a80
Verrgod <s10206142@connect.np.edu.sg>
2023-01-08 19:41:15 +0800

    Router for api/v1/settings

--- backend/src/routes/settings.js
+++ backend/src/routes/settings.js
@@ -1,2 +1,14 @@
+const express = require("express")
+const router = express.Router()
 
-//module.exports = router
+// middleware
+router.use((req, res, next) => {
+    console.log('Test')
+    next()
+})
+
+router.get('/', (req, res) => {
+    res.send('Settings')
+})
+
+module.exports = router
--- backend/src/server.js
+++ backend/src/server.js
@@ -14,6 +14,10 @@ app.get('/api/v1', (req, res) => {
 const companyRouter = require('./routes/companies')
 app.use('/api/v1/companies', companyRouter)
 
+// router for settings
+const settingsRouter = require('./routes/settings')
+app.use('/api/v1/settings', settingsRouter)
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 })
