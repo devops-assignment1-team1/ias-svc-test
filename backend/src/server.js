16c12408a9c802128cb11b77c01ac877673c9347
verrgod <lightsmitevernon@gmail.com>
2023-01-10 00:43:30 +0800

    nodemailer setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,6 +14,10 @@ app.get('/api/v1', (req, res) => {
 const companyRouter = require('./routes/companies')
 app.use('/api/v1/companies', companyRouter)
 
+// router for email
+const emailRouter = require('./routes/email')
+app.use('/api/v1/email', emailRouter)
+
 // router for settings
 const settingsRouter = require('./routes/settings')
 app.use('/api/v1/settings', settingsRouter)
